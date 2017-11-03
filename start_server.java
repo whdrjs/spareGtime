7e9c22a08b8e8854c2a9ea37238050082d8f0c48
asdfghjkkl11 <asdfghjkkl11@naver.com>
2017-11-03 19:39:57 +0900

    insert_room,insert_event,delete_room,delete_event,search_room

--- start_server.java
+++ start_server.java
@@ -2,15 +2,18 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.sql.PreparedStatement;
 import java.util.Scanner;
-package spareGtime
 public class start_server {
+	static class room_info {
+		static int count=0;
+		static int id[];
+		static String name[];
+	}
 	public static Scanner in = new Scanner(System.in);
 	
 	public static Connection getConnection()throws ClassNotFoundException,SQLException{
-		String url = "jdbc:mysql://localhost:3306/test";
+		String url = "jdbc:mysql://localhost:3306/sparegtime";
 		String user = "root";
 		String pass = "1234";
 		
@@ -20,15 +23,24 @@ public class start_server {
 		System.out.println("접속");
 		return conn;
 	}
-	public static void insert(String name,String id,String pw,int u_id) throws ClassNotFoundException,SQLException{
+	public static void insert_room(int ID,String name,int limit,int spare_time,String day,String content) throws ClassNotFoundException,SQLException{
 		Connection conn=getConnection();
-		String sql="insert into person(name,id,pw,u_id) values(?,?,?,?)";
+		String sql="insert into room values(?,?,?,?,?)";
 		PreparedStatement pstmt = conn.prepareStatement(sql);
 		
-		pstmt.setString(1,name);
-		pstmt.setString(2,id);
-		pstmt.setString(3,pw);
-		pstmt.setInt(4,u_id);	
+		//int ID
+		//String name
+		//int limit
+		//int spare_time
+		//String day
+		//String content
+		
+		pstmt.setInt(1,ID);	
+		pstmt.setString(2,name);
+		pstmt.setInt(3,limit);
+		pstmt.setInt(4,spare_time);
+		pstmt.setString(5,day);	
+		pstmt.setString(6,content);	
 		int res = pstmt.executeUpdate();
 		if(res>0)
 			System.out.println("처리");
@@ -36,57 +48,136 @@ public class start_server {
 		if(pstmt!=null)pstmt.close();
 		if(conn!=null)conn.close();
 	}
-	public static String search_id(String name,int u_id) throws ClassNotFoundException,SQLException{
+	public static void insert_event(String content,String name,double latitude,double longitude,float star,String address,String phone ) throws ClassNotFoundException,SQLException{
 		Connection conn=getConnection();
-		String sql="select id from person where name = ? and u_id = ?";
+		String sql="insert into contents values(?,?,?,?,?,?,?)";
 		PreparedStatement pstmt = conn.prepareStatement(sql);
-		String id=null;
+		
+		//String content 
+		//String name 
+		//double latitude
+		//double longitude
+		//float star
+		//String address
+		//String phone	
+	
+		pstmt.setString(1,content);
+		pstmt.setString(2,name);
+		pstmt.setDouble(6,latitude);
+		pstmt.setDouble(7,longitude);
+		pstmt.setFloat(4,star);
+		pstmt.setString(3,address);
+		pstmt.setString(5,phone);
+			
+		int res = pstmt.executeUpdate();
+		if(res>0)
+			System.out.println("처리");
+		
+		if(pstmt!=null)pstmt.close();
+		if(conn!=null)conn.close();
+	}
+	public static void delete_room(int ID) throws ClassNotFoundException,SQLException{
+		Connection conn=getConnection();
+		String sql="delete from room where ID = ?";
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		
+		pstmt.setInt(1,ID);
+
+		int res = pstmt.executeUpdate();
+		if(res>0)
+			System.out.println("처리");
+		
+		if(pstmt!=null)pstmt.close();
+		if(conn!=null)conn.close();
+	}
+	public static void delete_event(String name) throws ClassNotFoundException,SQLException{
+		Connection conn=getConnection();
+		String sql="delete from contents where name = ?";
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		
 		pstmt.setString(1,name);
-		pstmt.setInt(2,u_id);	
+
+		int res = pstmt.executeUpdate();
+		if(res>0)
+			System.out.println("처리");
+		
+		if(pstmt!=null)pstmt.close();
+		if(conn!=null)conn.close();
+	}
+	public static room_info search_room(int spare_time,String day,String content) throws ClassNotFoundException,SQLException{
+		Connection conn=getConnection();
+		room_info inform= new room_info();
+		String sql="select ID,name from room where spare_time = ? and day = ? and content = ?";
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		pstmt.setInt(1,spare_time);
+		pstmt.setString(2,day);
+		pstmt.setString(3,content);
+			
 		ResultSet res = pstmt.executeQuery();
 		if(res!=null)
 			System.out.println("탐색완료");
 		else {
-			id="정보에 맞는 id가 없습니다";
-			return id;
+			room_info.name[room_info.count]="정보에 맞는 방이 없습니다";
+			return inform;
 		}
 		while(res.next()) {
-			id=res.getString("id");
+			room_info.id[room_info.count]=res.getInt("ID");
+			room_info.name[room_info.count]=res.getString("name");
+			room_info.count++;
 		}
 		if(pstmt!=null)pstmt.close();
 		if(conn!=null)conn.close();
-		return id;
+		return inform;
 	}
-	public static String search_pw(String name,String id,int u_id) throws ClassNotFoundException,SQLException{
+	
+	public static String[] search_content(String main) throws ClassNotFoundException,SQLException{
 		Connection conn=getConnection();
-		String sql="select pw from person where name = ? and id = ? and u_id = ?";
+		String sql="select content from category where main = ?";
 		PreparedStatement pstmt = conn.prepareStatement(sql);
-		String pw=null;
-		pstmt.setString(1,name);
-		pstmt.setString(2,id);
-		pstmt.setInt(3,u_id);	
+		pstmt.setString(1,main);
+		String name[]=null;	
+		int count=0;
 		ResultSet res = pstmt.executeQuery();
 		if(res!=null)
 			System.out.println("탐색완료");
 		else {
-			pw="정보에 맞는 password가 없습니다";
-			return id;
+			name[0]= "정보에 맞는 컨텐츠가 없습니다";
+			return name;
 		}
 		while(res.next()) {
-			pw=res.getString("pw");
+			name[count]=res.getString("name");
+			count++;
 		}
 		if(pstmt!=null)pstmt.close();
 		if(conn!=null)conn.close();
-		return pw;
+		return name;
 	}
+	
 	public static void main(String[] args) throws ClassNotFoundException,SQLException{
-		String name = "손승범";
-		String id = null;
-		String pw = null;
-		int u_id = 201433689;	
-		id=search_id(name,u_id);
-		System.out.println(id);
-		pw=search_pw(name,id,u_id);
-		System.out.println(pw);
+		int ID=1234;
+		String R_name="pc방 갈사람";
+		int limit=4;
+		int spare_time=10;
+		String day="화";
+		String R_content="pc방";
+		//String content 
+		//String name 
+		//double latitude
+		//double longitude
+		//float star
+		//String address
+		//String phone	
+		String E_content ="이벤트";
+		String E_name="학과의날";
+		double latitude=37.4482461;
+		double longitude=127.1270569;
+		float star=5;
+		String address="IT대학 ";
+		String phone="010-1234-5678";
+		
+		insert_room(ID,R_name,limit,spare_time,day,R_content);
+		delete_room(ID);
+		insert_event(E_content ,E_name,latitude,longitude,star,address,phone);
+		delete_event(E_name);
 	}
 }
