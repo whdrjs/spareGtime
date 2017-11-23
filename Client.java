d6d643b4d1d0e5305dc37db0f0daf6b88104ba29
Jongkeon <30586378+whdrjs@users.noreply.github.com>
2017-11-23 22:50:15 +0900

    Update Client.java

--- Client.java
+++ Client.java
@@ -47,12 +47,12 @@ public class Client {
 		userIn.setVisible(true);	//can be show frame
 
 		//String serverAddress = UI.getServerAddress();	//type using getAddress method
-		chatSocket = new Socket("128.0.0.1", 9001); //채팅소켓을 만듬
+		chatSocket = new Socket("127.0.0.1", 9000); //채팅소켓을 만듬
 		in = new BufferedReader(new InputStreamReader(
 				chatSocket.getInputStream()));	//receive from server, message.
 		out = new PrintWriter(chatSocket.getOutputStream(), true); //send to server.
 
-		dataSocket=new Socket("128.0.0.1", 9002); //data 보내고 받을 소켓을 만듬
+		dataSocket=new Socket("127.0.0.1", 9000); //data 보내고 받을 소켓을 만듬
 		outToServer=new DataOutputStream(dataSocket.getOutputStream());
 		inFromServer = new BufferedReader(new InputStreamReader(
 				dataSocket.getInputStream()));	
@@ -63,37 +63,7 @@ public class Client {
 		//String storeInfo = ""; //서버에서 한 스트링으로 넘겨주니까 (가게 1, 가게 2 ,가게3, 가게 4)
 		// run 안에서는 채팅만 하게
 		//chatting-> chatServer에 넣을지 Server 에서 합칠지, 그런데 후자가 더 바람직할거 같아.
-		/*
-	         if(result.equals("SUBMITNAME")){ //서버로 부터 읽어온 값이 SUBMITNAME이면
-	            chat.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
-	            chat.frame.setVisible(true); //Chatclient에서 만든 GUI를 보여줌
-	            name = chat.getName(); //이름을 입력받음
-	            if(name.equals("")){
-	            	JOptionPane.showMessageDialog(null, "Please input your name!", "No name",
-							JOptionPane.WARNING_MESSAGE);
-	            	name = chat.getName();
-	            }
-	            chat.textName.setText(name); //이름을 창에 보여줌
-	            out.println(name); //서버로 이름을 보내줌
-	         } else if (result.startsWith("NAMEACCEPTED")) { //서버로 부터 읽어온 값이 NAMEACCEPT로 시작하면
-	            chat.textField.setEditable(true); //textField가 수정가능하게 바꿈
-	         } else if (result.startsWith("ENTRANCE")) {  //서버로 부터 읽어온 값이 ENTRANCE로 시작하면
-	            chat.textName.setEditable(false); //이제 textField 수정못하게 바꿈
-	            text = result.substring(8); // ENTRANCE 뒤부터 메세지이기 때문에 
-	            chat.messageArea.append("<" +text+ ">" + "님이 입장하셨습니다.\n"); //messageArea에 메세지를 보여줌
 
-	         } else if (result.startsWith("WHISPER")) {   //서버로 부터 읽어온 값이 WHISPER로 시작하면
-	            text = result.substring(7); // WHISPER 뒤부터 메세지이기 때문에 
-	            chat.messageArea.append(text);//messageArea에 메세지를 보여줌
-	         } else if (result.startsWith("MESSAGE")) {//서버로 부터 읽어온 값이 MESSAGE로 시작하면
-	            text = result.substring(8); // MESSAGE 뒤부터 메세지이기 때문에 
-	            chat.messageArea.append(text+"\n"); //messageArea에 메세지를 보여줌
-	         } else if (result.startsWith("EXIT")) {
-
-	            text = result.substring(4); // EXIT 뒤부터 이름이기 때문에 
-	            chat.messageArea.append("<" +text+ ">" + "님이 나가셨습니다.\n"); //messageArea에 메세지를 보여줌
-	         }
-		 */
 
 	}
 	//UI 에서 가져오고 서버로 보내고  가게리스트를 받아오는 메소드
