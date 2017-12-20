2a6bfa5fa7a4b3a47cefdaa5528b04d44933ba26
손승범 <32606420+asdfghjkkl11@users.noreply.github.com>
2017-12-20 21:01:42 +0900

    Update UI.java

--- UI.java
+++ UI.java
@@ -1,36 +1,22 @@
-package spareTime;
+
 /**
  * This class covers the GUI design and events of the program.
  * Form information and chatting to users.
  */
-import java.awt.BorderLayout;
+package spareTime;
+
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-
-import javax.swing.ButtonGroup;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
-import javax.swing.JComboBox;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JRadioButton;
-import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
-import javax.swing.ListSelectionModel;
-import javax.swing.JList;
-
-import spareTime.Client;
 
 
 
